7e91e714cea8017ed1c72d6e22f1c261079ae72c
Erick Borges <erickborges1230@gmail.com>
2025-07-04 14:35:30 -0300

    first commit

---
+++ src/main/java/Java_MongoDB/Funcionarios/Service/FuncionarioService.java
@@ -0,0 +1,9 @@
+package Java_MongoDB.Funcionarios.Service;
+
+import Java_MongoDB.Funcionarios.Enty.Funcionario;
+
+import java.util.List;
+
+public interface FuncionarioService {
+    List<Funcionario> listAll(); //Criando uma lista de funcionários
+}
---
+++ src/main/java/Java_MongoDB/Funcionarios/Service/FuncionariosServiceImplement.java
@@ -0,0 +1,25 @@
+package Java_MongoDB.Funcionarios.Service;
+
+import Java_MongoDB.Funcionarios.Enty.Funcionario;
+import Java_MongoDB.Funcionarios.Funcionario_Application;
+import Java_MongoDB.Funcionarios.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FuncionariosServiceImplement implements FuncionarioService {
+
+    //Criando uma injeção de dependência do FuncionarioRepository
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+    @Override
+    public List<Funcionario> listAll(){
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+        return funcionarios;
+    }
+
+}
