6aadb41755e61bab2e12efc1caea3393d55e757d
Erick Borges <erickborges1230@gmail.com>
2025-07-08 20:24:18 -0300

    first commit

--- src/main/java/Java_MongoDB/Funcionarios/Funcionario_Application.java
+++ src/main/java/Java_MongoDB/Funcionarios/Funcionario_Application.java
@@ -2,8 +2,10 @@ package Java_MongoDB.Funcionarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"FuncionarioRepository", "FuncionarioService"})
 public class Funcionario_Application {
 
 	public static void main(String[] args) {
--- src/main/java/Java_MongoDB/Funcionarios/repository/FuncionarioRepository.java
+++ src/main/java/Java_MongoDB/Funcionarios/repository/FuncionarioRepository.java
@@ -2,10 +2,11 @@ package Java_MongoDB.Funcionarios.repository;
 
 import Java_MongoDB.Funcionarios.Enty.Funcionario;
 import org.bson.types.ObjectId;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-
+@ComponentScan
 @Repository
 public interface FuncionarioRepository extends MongoRepository <Funcionario, ObjectId> { //Tá recebendo a Enty + obeject ID
 }
